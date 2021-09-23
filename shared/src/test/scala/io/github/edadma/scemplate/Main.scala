e404276822f9bc0d2c58c9486d7884bcb46949b9
Edward A Maxedon, Sr <edadma@gmail.com>
2021-09-22 20:36:19 -0400

    cont coding tag parser

--- shared/src/test/scala/io/github/edadma/scemplate/Main.scala
+++ shared/src/test/scala/io/github/edadma/scemplate/Main.scala
@@ -4,18 +4,44 @@ import scala.util.{Failure, Success}
 import org.parboiled2._
 import pprint._
 
-import scala.language.implicitConversions
+import scala.language.{implicitConversions, postfixOps}
 
 object Main extends App {
 
-  val tag = " a + 2 * b "
+  val tag = " abc "
 
   val parser = new TagParser(tag)
 
   parser.tag.run() match {
-    case Success(ast)           => pprintln(ast)
+    case Success(ast) =>
+      pprintln(ast)
+
+      val p = ast.asInstanceOf[VarExpr].ident.pos
+
+      problem(p, parser, "asdf")
     case Failure(e: ParseError) => println("Expression is not valid: " + parser.formatError(e))
     case Failure(e)             => println("Unexpected error during parsing run: " + e)
+  }
+
+  def problem(pos: Position, parser: Parser, msg: String): Nothing = {
+    Console.err.println(new RuntimeErrorFormatter(msg).customFormat(ParseError(pos, pos, Nil), parser.input))
+    sys.exit(1)
+  }
+
+}
+
+class RuntimeErrorFormatter(msg: String) extends ErrorFormatter {
+
+  import java.lang.{StringBuilder => JStringBuilder}
+
+  def customFormat(error: ParseError, input: ParserInput): String = {
+    import error._
+
+    val sb = new JStringBuilder(128)
+
+    sb.append(msg)
+    sb.append(" (line ").append(position.line).append(", column ").append(position.column).append(')')
+    formatErrorLine(sb.append(':').append('\n'), error, input).toString
   }
 
 }
@@ -23,6 +49,8 @@ object Main extends App {
 class TagParser(val input: ParserInput) extends Parser {
 
   implicit def wspStr(s: String): Rule0 = rule(str(s) ~ zeroOrMore(' '))
+
+  def pos: Position = Position(cursor, input)
 
   def tag: Rule1[ExprAST] = rule(ws ~ expression ~ EOI)
 
@@ -49,11 +77,16 @@ class TagParser(val input: ParserInput) extends Parser {
 
   def variable: Rule1[VarExpr] = rule(ident ~> VarExpr)
 
-  def ident: Rule1[String] = rule(capture((CharPredicate.Alpha | '_') ~ zeroOrMore(CharPredicate.AlphaNum | '_')) ~ ws)
+  def ident: Rule1[Ident] =
+    rule {
+      push(pos) ~ capture((CharPredicate.Alpha | '_') ~ zeroOrMore(CharPredicate.AlphaNum | '_')) ~> Ident ~ ws
+    }
 
   def ws: Rule0 = rule(zeroOrMore(' '))
 
 }
+
+case class Ident(pos: Position, name: String)
 
 trait AST
 
@@ -61,7 +94,7 @@ trait ExprAST
 
 case class ValueExpr(value: String) extends ExprAST
 
-case class VarExpr(ident: String) extends ExprAST
+case class VarExpr(ident: Ident) extends ExprAST
 
 case class AddExpr(left: ExprAST, right: ExprAST) extends ExprAST
 
