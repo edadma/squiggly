36984a70995e7916989549496517e71cf4f794f6
Edward A Maxedon, Sr <edadma@gmail.com>
2021-09-28 12:41:55 -0400

    unit tests

--- shared/src/test/scala/io/github/edadma/scemplate/LogicTests.scala
+++ shared/src/test/scala/io/github/edadma/scemplate/LogicTests.scala
@@ -10,43 +10,43 @@ class LogicTests extends AnyFreeSpec with Matchers with Testing {
   }
 
   "logic 2" in {
-    test(null, "{{ -3 + 4 }}") shouldBe "1"
+    test(null, "{{ true and false }}") shouldBe "false"
   }
 
   "logic 3" in {
-    test(null, "{{ 3 + -4 }}") shouldBe "-1"
+    test(null, "{{ false and true }}") shouldBe "false"
   }
 
   "logic 4" in {
-    test(null, "{{ -3 + -4 }}") shouldBe "-7"
+    test(null, "{{ false and false }}") shouldBe "false"
   }
 
   "logic 5" in {
-    test(null, "{{ 3 * 4 }}") shouldBe "12"
+    test(null, "{{ false or false }}") shouldBe "false"
   }
 
   "logic 6" in {
-    test(null, "{{ 3 / 4 }}") shouldBe "0.75"
+    test(null, "{{ false or true }}") shouldBe "true"
   }
 
   "logic 7" in {
-    test(null, "{{ 10 \\ 4 }}") shouldBe "2"
+    test(null, "{{ true or true }}") shouldBe "true"
   }
 
   "logic 8" in {
-    test(null, "{{ 7 mod 4 }}") shouldBe "3"
+    test(null, "{{ true or false }}") shouldBe "true"
   }
 
   "logic 9" in {
-    test(null, "{{ 3 ^ 4 }}") shouldBe "81"
+    test(null, "{{ true or true and true }}") shouldBe "true"
   }
 
   "logic 10" in {
-    test(null, "{{ 2 ^ 3 ^ 3 }}") shouldBe "134217728"
+    test(null, "{{true or true and false }}") shouldBe "true"
   }
 
   "logic 11" in {
-    test(null, "{{ 3 - 4 }}") shouldBe "-1"
+    test(null, "{{(true or true) and false }}") shouldBe "false"
   }
 
   "logic 12" in {
