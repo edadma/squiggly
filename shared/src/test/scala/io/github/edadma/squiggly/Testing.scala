dfd356106d6fa92a37009cf70fa2e37708a809d7
Edward A Maxedon, Sr <edadma@gmail.com>
2021-10-02 09:29:30 -0400

    unit tests

--- shared/src/test/scala/io/github/edadma/squiggly/Testing.scala
+++ shared/src/test/scala/io/github/edadma/squiggly/Testing.scala
@@ -5,7 +5,7 @@ import java.io.{ByteArrayOutputStream, PrintStream}
 trait Testing {
 
   def test(yaml: String, template: String): String = {
-    val data = if (yaml eq null) Map() else platform.yaml(yaml)
+    val data = if (yaml eq null) null else platform.yaml(yaml)
     val parser = new TemplateParser(template, "{{", "}}", Builtin.functions, Builtin.namespaces)
     val ast = parser.parse
     val buf = new ByteArrayOutputStream
